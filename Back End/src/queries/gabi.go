c48bfcca5c87335bb8c7881735adda790fa60262
ZiGabiZi <109861552+ZiGabiZi@users.noreply.github.com>
2024-05-22 14:58:57 +0300

    Tokes + Comments

--- Back End/src/queries/gabi.go
+++ Back End/src/queries/gabi.go
@@ -9,9 +9,24 @@ import (
 func Route_gabi(router gin.IRouter) {
 	router.GET("/gabi", gabi.GABI)
 	router.GET("/exemplu1", gabi.GetEXEMPLU1)
+	/*
+		Inserarea unei noi școli în baza de date
+		Primesc ca parametrii din POST: nume_scoala, adresa, telefon
+	*/
 	router.POST("/inserareScoala", gabi.Inregistrare_Instit_Invat)
+	/*
+		Inserarea unei noi clase în baza de date
+		Primesc ca parametrii din POST: id_scoala, csv_file (fișier .CSV)
+	*/
 	router.POST("/inserareClasa", gabi.Info_clase)
+	/*
+		Inserarea unui nou profesor în baza de date
+		Primesc ca parametrii din POST: id_scoala, nume, prenume, materie, email
+	*/
 	router.POST("/inserareProfesor", gabi.Info_profesor)
+	/*
+		Inserarea unui nou elev în baza de date
+		Primesc ca parametrii din POST: id_scoala, nume, prenume, clasa, email
+	*/
 	router.POST("/inserareElev", gabi.Info_elev)
-
 }
