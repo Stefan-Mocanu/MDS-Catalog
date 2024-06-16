1eb55836d75834498eee32f68513a7eefa28a84d
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2024-06-16 22:42:02 +0300

    mici comentarii

--- Back End/src/queries/stefan.go
+++ Back End/src/queries/stefan.go
@@ -102,36 +102,49 @@ func Route_stefan(router gin.IRouter) {
 		Primeste ca parametrii din GET: id_scoala, id_clasa
 	*/
 	router.GET("/getMaterii", stefan.GetMaterii)
+	
+	//mai tarziu
 	/*
 		Obtinerea parametrilor de realizare a unui grafic barplot pentru comparatia mediilor pana la o anumita data si dupa aceasta
 		Primeste ca parametrii din GET: id_scoala, id_clasa, materie, data
 		Acest grafic este pentru profesor
 	*/
 	router.GET("/getEvolutie", stefan.GetEvolutie)
+	
+	//mai tarziu
 	/*
 		Obtinerea parametrilor de realizare a unui grafic boxplot pentru vizualizarea notelor si activitatii
 		Primeste ca parametrii din GET: id_scoala, id_clasa, materie
 		Acest grafic este pentru profesor
 	*/
 	router.GET("/getNoteActivitate", stefan.GetBoxNoteActivitate)
+	
+	//facut
 	/*
 		Obtinerea parametrilor de realizare a unui grafic pentru vizualizarea mediilor la fiecare materie si locul in clasamentul clasei
 		Primeste ca parametrii din GET: id_scoala, id_clasa, id_elev
 		Acest grafic este pentru parinte
 	*/
 	router.GET("/GetMedieClasament", stefan.GetMedieClasament)
+	
+	//facut
 	/*
 		Obtinerea parametrilor de realizare a unui grafic sunburst pentru vizualizarea incadrarii profesorale
 		Primeste ca parametrii din GET: id_scoala
 		Acest grafic este pentru admin
 	*/
 	router.GET("/GetSunBurstIncadrare", stefan.GetSunBurstIncadrare)
+	
+	
+	
 	/*
 		Obtinerea parametrilor de realizare a unui grafic pentru vizualizarea fiecarui elev din scoala cu numarul de absente si media sa, separati de etnii
 		Primeste ca parametrii din GET: id_scoala
 		Acest grafic este pentru admin
 	*/
 	router.GET("/GetScatterMediiAbsente", stefan.GetGraficMediiEtnii)
+	
+	
 	/*
 		Obtinerea parametrilor de realizare a unui grafic funnel cu mediile din scoala
 		Primeste ca parametrii din GET: id_scoala
@@ -144,6 +157,9 @@ func Route_stefan(router gin.IRouter) {
 		Acest grafic este pentru admin
 	*/
 	router.GET("/GetHeatmapMediiActiv", stefan.GetHeatMapMediiActiv)
+	
+	
+	//facut
 	/*
 		Obtinerea parametrilor de realizare a unui grafic pentru fiecare materie cu evolutia mediei de acum 30 de zile si in prezent
 		Primeste ca parametrii din GET: id_scoala, id_clasa, id_elev
